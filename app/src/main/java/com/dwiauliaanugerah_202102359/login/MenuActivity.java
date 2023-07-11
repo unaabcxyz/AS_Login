ac7b5c332cac2ed0f0c1a745698b357561e0965e
unaabcxyz <tkimhyung1230@gmail.com>
2023-07-11 10:57:13 +0700

    login forex

--- app/src/main/java/com/dwiauliaanugerah_202102359/login/MenuActivity.java
+++ app/src/main/java/com/dwiauliaanugerah_202102359/login/MenuActivity.java
@@ -2,13 +2,49 @@ package com.dwiauliaanugerah_202102359.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity 
+{
+    private Button _tampilMahasiswaButton, _tampilForexButton;
+    private Intent _tampilMahasiswaIntent, _tampilForexIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        
+        initTampilMahasiswaButton();
+        initTampilForexButton();
+    }
+
+    private void initTampilForexButton()
+    {
+        _tampilForexButton = (Button) findViewById(R.id.tampilForexButton);
+        _tampilForexButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                _tampilForexIntent = new  Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexIntent);
+            }
+        });
+
+    }
+
+    private void initTampilMahasiswaButton()
+    {
+        _tampilMahasiswaButton = (Button) findViewById(R.id.tampilMahasiswaButton);
+
+        _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(_tampilMahasiswaIntent);
+            }
+        });
     }
 }
